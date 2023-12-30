cd72a0374a60fcb6406aa2f7a93c7cc09b042d92
Giuseppe Cramarossa <16326537+gcramarossa@users.noreply.github.com>
2023-12-30 15:11:01 +0100

    feat first example added

---
+++ basic/BasicCommand/BasicCommand.CLI/Program.cs
@@ -0,0 +1,18 @@
+﻿using InterAppConnector;
+using InterAppConnector.DataModels;
+using BasicCommand.Library;
+
+namespace HelloApplication.CLI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandManager command = new CommandManager();
+            command.AddCommand<HelloCommand, EmptyDataModel>();
+
+            InterAppCommunication connector = new InterAppCommunication(command);
+            connector.ExecuteAsInteractiveCLI(args);
+        }
+    }
+}
---
+++ basic/BasicCommand/BasicCommand.Library/HelloCommand.cs
@@ -0,0 +1,16 @@
+﻿using InterAppConnector;
+using InterAppConnector.Attributes;
+using InterAppConnector.DataModels;
+using InterAppConnector.Interfaces;
+
+namespace BasicCommand.Library
+{
+    [Command("hello", Description = "A simple hello world command")]
+    public class HelloCommand : ICommand<EmptyDataModel>
+    {
+        public string Main(EmptyDataModel arguments)
+        {
+            return CommandOutput.Ok("Hello World");
+        }
+    }
+}
