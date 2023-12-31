30da12dab38a210ebb41174d2415ae1df157fb35
Giuseppe Cramarossa <16326537+gcramarossa@users.noreply.github.com>
2023-12-31 17:04:35 +0100

    feat added web and gui example in basic example

---
+++ basic/BasicCommand/BasicCommand.MAUI/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using BasicCommand.Library;
+using InterAppConnector.DataModels;
+using InterAppConnector;
+using System.Dynamic;
+
+namespace BasicCommand.MAUI
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            CommandResult<string> commandResult = InterAppCommunication.CallSingleCommand<HelloCommand, EmptyDataModel>()
+                .ExecuteAsBatch<string>("hello", new ExpandoObject());
+
+            MessageStatus.Text = "MessageStatus: " + Enum.GetName(commandResult.MessageStatus);
+            MessageType.Text = "MessageType: " + commandResult.MessageType;
+            Message.Text = "Message: " + commandResult.Message;
+        }
+    }
+
+}
---
+++ basic/BasicCommand/BasicCommand.Web/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using BasicCommand.Library;
+using BasicCommand.Web.Models;
+using InterAppConnector;
+using InterAppConnector.DataModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Dynamic;
+
+namespace BasicCommand.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        protected CommandResult<string> commandResult = null;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            commandResult = InterAppCommunication.CallSingleCommand<HelloCommand, EmptyDataModel>().ExecuteAsBatch<string>("hello", new ExpandoObject());
+            return View(commandResult);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
